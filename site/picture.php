ff69e8604efafe132c770a985b8d991d37f4dc07
Edson <edsonobando18@gmail.com>
2020-03-07 15:40:07 -0600

    Template one

--- site/picture.php
+++ site/picture.php
@@ -3,51 +3,63 @@ require_once './shared/header.php';
 require_once './shared/guard.php';
 require_once './shared/db.php';
 ?>
+
 <link rel="stylesheet" type="text/css" href="./css/style.css">
-<?php 
+<?php
+	$id_curriculum = $_GET['id_curriculum'];
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		$my_image = $_FILES;
-			if(isset($_POST['ready'])){
+		if(isset($_POST['ready'])){
+			if(!empty($_FILES['image']['name'])){
 				$id_user = $_SESSION['user_id'];
 				$ruta = 'Images/'.$_FILES['image']['name'];
 				move_uploaded_file($_FILES['image']['tmp_name'],$ruta);
 				$sql = 'insert into images (id_user,URL) VALUES ($1,$2)';
 				$results = $con->runQuery($sql,[$id_user,$ruta]);
-			}else if(isset($_POST['visualize'])){
-				$ruta = 'Images/'.$_FILES['image']['name'];
-				move_uploaded_file($_FILES['image']['tmp_name'],$ruta);
 			}
-			
-		}
+		}else{
+			header("Location: ./template_one.php?id_curriculum=$id_curriculum");
+		}		
+	}
  ?>
-<div class="all">
-<div class="container col-lg-6 col-lg-offset-2" id="pictureAll">
-	<div class="panel panel-default" id="myPanel">
-		<div class="panel-heading">
-			<div class="panel-title">
-				<center>
-					<h1>Select your profile picture</h1>
-				</center>
-				
+<section class="section">
+	<div class="all">
+	<div class="container col-lg-6 col-lg-offset-2" id="pictureAll">
+		<div class="panel panel-default" id="myPanel">
+			<div class="panel-heading">
+				<div class="panel-title">
+					<center>
+						<h1>Choose your profile image</h1>
+					</center>
+					
+				</div>
+			</div>
+			<div class="panel-body">
+				<form method="post" enctype="multipart/form-data">
+					<div class="form-group">
+						<input type="file" name="image">
+					</div>
+					<?php if($ruta != ''){ ?>
+						<div class="form-group">
+							<img src="<?php echo $ruta ?>" id="myImage">
+						</div>
+					<?php } ?>
+					<div id="btn_finalizar">
+						<button class="btn btn-success" name="ready" >View and choose image</button>
+					</div>
+				</form>
 			</div>
 		</div>
-		<div class="panel-body">
-			<form method="post" enctype="multipart/form-data">
-				<div class="form-group">
-					<input type="file" name="image" >
-				</div>
-				<button class="btn btn-success" name="visualize">Visualize picture</button>
-				<?php if($ruta != ''){ ?>
-					<div class="form-group">
-						<img src="<?php echo $ruta ?>" id="myImage">
+	</div>
+	</div>
+	<?php if($ruta != ''){ ?>
+	<div class="almacen">
+		<div id="btn_next_pic">
+				<form method="POST">
+					<div >
+						<button class="btn btn-success">Finish and create templates</button>
 					</div>
-					<div id="btn_finalizar">
-						<button class="btn btn-success" name="ready" >Save and finish</button>
-					</div>
-				<?php } ?>
-
-			</form>
+				</form>
 		</div>
 	</div>
-</div>
-</div>
+	<?php } ?>
+</section>
